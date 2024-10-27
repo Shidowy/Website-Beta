6855cb98786c2fe497587153fea7a87b50999a81
Shidowy <101477459+Shidowy@users.noreply.github.com>
2024-10-27 16:33:06 -0700

    setup DB, backend and frontend connection!

--- nt-frontend/src/App.tsx
+++ nt-frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
 import About from './pages/about';
@@ -6,9 +6,24 @@ import OurTeam from './pages/team';
 import Courses from './pages/courses';
 import Impact from './pages/impact';
 import GetInvolved from './pages/get-involved';
-
+import axios from 'axios';
 
 const App: React.FC = () => {
+  const [message, setMessage] = useState<string>('');
+
+  useEffect(() => {
+    const fetchHelloWorld = async () => {
+      try {
+        const response = await axios.get('http://127.0.0.1:8000/api/hello/');
+        setMessage(response.data.message);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchHelloWorld();
+  }, []);
+
   return (
     <Router>
       <main>
@@ -20,6 +35,7 @@ const App: React.FC = () => {
           <Route path="/impact" element={<Impact />} />
           <Route path="/get-involved" element={<GetInvolved />} />
         </Routes>
+        <p>{message}</p>
       </main>
     </Router>
   );
